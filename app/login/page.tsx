d0e57bad22678878eec8a46ebd79cbed23a88dc6
niLPotential <niLPotential@proton.me>
2024-02-15 07:04:06 +0900

    Add basic login page

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <form>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+  );
+}

302bea20c7b6db35ef5e1e2418fb3784971edc87
niLPotential <niLPotential@proton.me>
2024-02-15 19:53:59 +0900

    Add tabs to account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,3 +1,18 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <Tabs>
+      <TabsList>
+        <TabsTrigger value="profile">Profile</TabsTrigger>
+        <TabsTrigger value="tokens-payments">Tokens/Payments</TabsTrigger>
+        <TabsTrigger value="library">Library</TabsTrigger>
+      </TabsList>
+      <TabsContent value="profile">Name, Account info etc.</TabsContent>
+      <TabsContent value="tokens-payments">
+        Tokens, Subscription etc.
+      </TabsContent>
+      <TabsContent value="library">Library of generated music</TabsContent>
+    </Tabs>
+  );
 }

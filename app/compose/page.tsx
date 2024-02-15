c9d7e0336b625026f9feced90c4784aafc5f5410
niLPotential <niLPotential@proton.me>
2024-02-15 14:49:50 +0900

    Add form input for theme, length, bpm, genre

--- app/compose/page.tsx
+++ app/compose/page.tsx
@@ -1,12 +1,128 @@
-export default function Page() {
-  return (
-    <form>
-      <textarea></textarea>
-      <input type="number" min={0} />
-      <input type="number" min={0} />
-      <input type="file" accept="audio/*" />
+"use client";
 
-      <input type="file" accept="image/*" />
-    </form>
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Slider } from "@/components/ui/slider";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+
+const formSchema = z.object({
+  theme: z.string().min(10).max(100),
+  length: z.coerce.number().positive().int().max(60),
+  bpm: z.coerce.number().positive().int().array().length(1),
+  genre: z.string(),
+});
+
+export default function Page() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      theme: "",
+      length: 30,
+      bpm: [160],
+      genre: "",
+    },
+  });
+
+  function onSubmit(value: z.infer<typeof formSchema>) {
+    console.log(value);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="theme"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Theme</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Enter prompts to generate music"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>Example</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="length"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Length</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="30" {...field} />
+              </FormControl>
+              <FormDescription>music length</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="bpm"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>BPM</FormLabel>
+              <FormControl>
+                <Slider
+                  min={80}
+                  max={240}
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                />
+              </FormControl>
+              <FormDescription>bpm</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="genre"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Genre</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a genre" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="rock">Rock</SelectItem>
+                  <SelectItem value="pop">Pop</SelectItem>
+                  <SelectItem value="jazz">Jazz</SelectItem>
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }

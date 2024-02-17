d1cd1033a90a89e00674a1edb0feb96f50bae635
niLPotential <niLPotential@proton.me>
2024-02-17 13:34:33 +0900

    Add radio group to tokens page

--- app/shop/tokens/page.tsx
+++ app/shop/tokens/page.tsx
@@ -1,3 +1,21 @@
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
 export default function Page() {
-  return <div>Tokens page</div>;
+  return (
+    <RadioGroup defaultValue="option-one">
+      <div>
+        <RadioGroupItem value="option-one" id="option-one" />
+        <Label htmlFor="option-one">Option One</Label>
+      </div>
+      <div>
+        <RadioGroupItem value="option-two" id="option-two" />
+        <Label htmlFor="option-two">Option Two</Label>
+      </div>
+      <div>
+        <RadioGroupItem value="option-three" id="option-three" />
+        <Label htmlFor="option-three">Option Three</Label>
+      </div>
+    </RadioGroup>
+  );
 }

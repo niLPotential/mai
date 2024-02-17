e726f031b5330f6a715d717cd78ff2bf5243d654
niLPotential <niLPotential@proton.me>
2024-02-17 20:30:24 +0900

    Add alert to compose page for debugging

--- app/compose/page.tsx
+++ app/compose/page.tsx
@@ -47,112 +47,121 @@ export default function Page() {
   });
 
   function onSubmit(value: z.infer<typeof formSchema>) {
+    alert(`Input values are:
+    theme: ${value.theme}
+    length: ${value.length}
+    bpm: ${value.bpm},
+    genre: ${value.genre}
+    instruments: ${value.instruments}
+    `);
     console.log(value);
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="theme"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Theme</FormLabel>
-              <FormControl>
-                <Textarea
-                  required
-                  className="resize-none"
-                  placeholder="Enter prompts to generate music"
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>Example</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="length"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Length</FormLabel>
-              <FormControl>
-                <Input
-                  type="number"
-                  placeholder="30"
-                  min={1}
-                  max={60}
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>music length: {field.value}s</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="bpm"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>BPM</FormLabel>
-              <FormControl>
-                <Slider
-                  min={80}
-                  max={240}
+    <div className="p-10">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="theme"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Theme</FormLabel>
+                <FormControl>
+                  <Textarea
+                    required
+                    className="resize-none"
+                    placeholder="Enter prompts to generate music"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>Example</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="length"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Length</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    placeholder="30"
+                    min={1}
+                    max={60}
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>music length: {field.value}s</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="bpm"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>BPM</FormLabel>
+                <FormControl>
+                  <Slider
+                    min={80}
+                    max={240}
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  />
+                </FormControl>
+                <FormDescription>{field.value} bpm</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="genre"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Genre</FormLabel>
+                <Select
                   onValueChange={field.onChange}
                   defaultValue={field.value}
-                />
-              </FormControl>
-              <FormDescription>{field.value} bpm</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="genre"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Genre</FormLabel>
-              <Select
-                onValueChange={field.onChange}
-                defaultValue={field.value}
-                required
-              >
+                  required
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a genre" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="rock">Rock</SelectItem>
+                    <SelectItem value="pop">Pop</SelectItem>
+                    <SelectItem value="jazz">Jazz</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="instruments"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Instruments</FormLabel>
                 <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a genre" />
-                  </SelectTrigger>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
                 </FormControl>
-                <SelectContent>
-                  <SelectItem value="rock">Rock</SelectItem>
-                  <SelectItem value="pop">Pop</SelectItem>
-                  <SelectItem value="jazz">Jazz</SelectItem>
-                </SelectContent>
-              </Select>
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="instruments"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Instruments</FormLabel>
-              <FormControl>
-                <Switch
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
   );
 }

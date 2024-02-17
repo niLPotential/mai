934ea458c98b3019700040adc929d8e7949a0a6d
niLPotential <niLPotential@proton.me>
2024-02-17 13:27:50 +0900

    Add carousel to promotion page

--- app/shop/promotion/page.tsx
+++ app/shop/promotion/page.tsx
@@ -1,3 +1,21 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 export default function Page() {
-  return <div>Promotion page</div>;
+  return (
+    <Carousel>
+      <CarouselContent>
+        <CarouselItem>Promotion 1</CarouselItem>
+        <CarouselItem>Promotion 2</CarouselItem>
+        <CarouselItem>Promotion 3</CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
 }

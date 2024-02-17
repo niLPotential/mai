3d11bb2149fc500ac2d4faf8169a3e2f99ec5b0a
niLPotential <niLPotential@proton.me>
2024-02-17 18:43:14 +0900

    Add home example

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-1 flex-col p-5 md:px-72">
+      <h1 className="py-10 text-center text-6xl">Welcome to MAI</h1>
+      <h2 className="py-5 text-center text-4xl">
+        Using AI generated music for all
+      </h2>
+      <p className="py-2 text-2xl">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac aliquet
+        elit, nec malesuada ex. Suspendisse rhoncus mi massa, vel mollis nisl
+        ornare in. Phasellus lobortis arcu dui, vel consequat odio vulputate
+        laoreet. Etiam eu commodo leo. Nulla in metus vitae sapien posuere
+        ornare vel vel mauris. Sed sit amet volutpat enim, sit amet fermentum
+        justo. In aliquet dolor lorem, sit amet consequat ligula imperdiet non.
+      </p>
+    </div>
+  );
 }

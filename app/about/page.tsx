2d782e6c58fc123c940afebd6579984d1e08694b
niLPotential <niLPotential@proton.me>
2024-02-17 13:28:11 +0900

    Add lorem ipsum as dummy content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,51 @@
 export default async function Page() {
-  const data = await getData();
-
   return (
     <>
-      <h1>{data.title}</h1>
-      <p>{data.body}</p>;
+      <h1>About MAI</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut massa
+        felis. Praesent posuere, tellus dignissim luctus consequat, tortor urna
+        facilisis odio, quis finibus mauris diam non ipsum. Etiam in eleifend
+        sapien. Etiam vitae lacinia leo, a porta sem. Ut in venenatis lacus, eu
+        tristique felis. Aenean euismod placerat nisi auctor ultrices. Aliquam
+        tincidunt fringilla pellentesque. Fusce venenatis finibus ipsum,
+        scelerisque hendrerit risus aliquet sit amet. Proin lobortis vehicula
+        nibh, eu egestas purus egestas non. Suspendisse a placerat lacus.
+        Suspendisse potenti. Mauris ornare in ipsum a euismod. In mattis sapien
+        ligula, id volutpat leo mollis vel. Etiam quis velit dui. In lobortis,
+        purus ut semper imperdiet, dolor eros iaculis urna, nec sollicitudin ex
+        nibh a est. Proin porta maximus aliquet. Sed nec volutpat purus, id
+        laoreet felis. Suspendisse tincidunt lectus et magna tempor accumsan.
+        Praesent rutrum leo eget enim efficitur, bibendum ultricies purus
+        aliquet. Aenean non tempor mi. Aenean vitae nisi vitae urna tempor
+        tempus. Proin magna arcu, dapibus tempus augue mollis, mollis finibus
+        velit. Donec bibendum et quam et vestibulum. Maecenas scelerisque, velit
+        sed pellentesque elementum, lectus erat porttitor mi, non auctor dolor
+        quam in tortor. Mauris bibendum pellentesque orci. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Mauris molestie congue augue, quis
+        feugiat risus. In hac habitasse platea dictumst. Vestibulum at lacinia
+        purus. Mauris sit amet posuere felis. Sed iaculis gravida turpis, ac
+        pharetra nisi. Sed porttitor sit amet nibh id ullamcorper. Pellentesque
+        eget fermentum justo, eget consequat nibh. Ut tempus ligula et urna
+        rutrum consectetur. Quisque risus nisl, consequat sed leo vel, commodo
+        bibendum libero. Duis lectus nulla, dapibus vel justo sed, hendrerit
+        accumsan tellus. Nunc faucibus laoreet velit eu pharetra. Sed neque
+        enim, blandit vel leo fermentum, porttitor ultrices nunc. Suspendisse
+        dapibus ultricies mauris, sed porta tortor. Vestibulum ante ipsum primis
+        in faucibus orci luctus et ultrices posuere cubilia curae; Aenean nec
+        nibh ac nunc accumsan dictum. Suspendisse potenti. Nullam vestibulum
+        euismod consequat. Mauris id magna sit amet dolor vulputate convallis.
+        Donec et efficitur orci. Aliquam porta at mi et lobortis. Proin eget
+        tristique dui. Quisque ultricies porta nisl, a mollis neque tempus id.
+        Curabitur dapibus a dui ut porta. Nulla dapibus porta molestie. Integer
+        et imperdiet metus, ut scelerisque neque. Etiam molestie odio quis
+        rutrum luctus. Suspendisse placerat ullamcorper orci, vel tincidunt nunc
+        maximus non. Mauris fringilla id diam condimentum congue. Fusce ut nibh
+        molestie, lobortis velit eget, volutpat mi. Morbi ultrices mauris sit
+        amet felis efficitur, quis tristique arcu sollicitudin. Vestibulum massa
+        enim, mollis eu nulla pellentesque, eleifend sollicitudin risus.
+        Suspendisse eget pulvinar tortor.
+      </p>
     </>
   );
-}
-
-async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
 }

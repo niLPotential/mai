d8f319ffc2fda3ac6edda8251ffced6cb0b55c62
niLPotential <niLPotential@proton.me>
2024-02-17 20:23:36 +0900

    Style subscribe page

--- app/shop/subscribe/page.tsx
+++ app/shop/subscribe/page.tsx
@@ -9,19 +9,35 @@ import {
 
 export default function Page() {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Free Tier</CardTitle>
-        <CardDescription>Allows for 10 requests per week</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content 1</p>
-        <p>Card Content 2</p>
-        <p>Card Content 3</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
+    <div className="flex justify-center gap-5">
+      <Card>
+        <CardHeader>
+          <CardTitle>Free Tier</CardTitle>
+          <CardDescription>Allows for 10 requests per week</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Free Content 1</p>
+          <p>Free Content 2</p>
+          <p>Free Content 3</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Paid Tier</CardTitle>
+          <CardDescription>Allows for 100 requests per week</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Paid Content 1</p>
+          <p>Paid Content 2</p>
+          <p>Paid Content 3</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }

9d15d111a3f60fd2fa412f498e862afbacb3f621
niLPotential <niLPotential@proton.me>
2024-02-17 19:15:53 +0900

    Style login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/ui/input";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -12,25 +13,32 @@ import {
 
 export default function Page() {
   return (
-    <form>
-      <input type="email" name="email" placeholder="Email" required />
-      <input type="password" name="password" placeholder="Password" required />
-      <AlertDialog>
-        <AlertDialogTrigger type="submit">Sign In</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>This is currently in demo</AlertDialogTitle>
-            <AlertDialogDescription>
-              This site is a demo project. Signing in is currently not
-              supported.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </form>
+    <div className="flex flex-1 flex-col items-center">
+      <form className="flex max-w-80 flex-col items-center justify-center gap-3 rounded p-5 ring ring-ring">
+        <Input type="email" name="email" placeholder="Email" required />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        />
+        <AlertDialog>
+          <AlertDialogTrigger type="submit">Sign In</AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>This is currently in demo</AlertDialogTitle>
+              <AlertDialogDescription>
+                This site is a demo project. Signing in is currently not
+                supported.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </form>
+    </div>
   );
 }

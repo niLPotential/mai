e432c9e19363104e8ee973b3f918a06c5bff78f4
niLPotential <niLPotential@proton.me>
2024-02-17 18:48:25 +0900

    Add about page example text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,51 +1,35 @@
 export default async function Page() {
   return (
-    <>
-      <h1>About MAI</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut massa
-        felis. Praesent posuere, tellus dignissim luctus consequat, tortor urna
-        facilisis odio, quis finibus mauris diam non ipsum. Etiam in eleifend
-        sapien. Etiam vitae lacinia leo, a porta sem. Ut in venenatis lacus, eu
-        tristique felis. Aenean euismod placerat nisi auctor ultrices. Aliquam
-        tincidunt fringilla pellentesque. Fusce venenatis finibus ipsum,
-        scelerisque hendrerit risus aliquet sit amet. Proin lobortis vehicula
-        nibh, eu egestas purus egestas non. Suspendisse a placerat lacus.
-        Suspendisse potenti. Mauris ornare in ipsum a euismod. In mattis sapien
-        ligula, id volutpat leo mollis vel. Etiam quis velit dui. In lobortis,
-        purus ut semper imperdiet, dolor eros iaculis urna, nec sollicitudin ex
-        nibh a est. Proin porta maximus aliquet. Sed nec volutpat purus, id
-        laoreet felis. Suspendisse tincidunt lectus et magna tempor accumsan.
-        Praesent rutrum leo eget enim efficitur, bibendum ultricies purus
-        aliquet. Aenean non tempor mi. Aenean vitae nisi vitae urna tempor
-        tempus. Proin magna arcu, dapibus tempus augue mollis, mollis finibus
-        velit. Donec bibendum et quam et vestibulum. Maecenas scelerisque, velit
-        sed pellentesque elementum, lectus erat porttitor mi, non auctor dolor
-        quam in tortor. Mauris bibendum pellentesque orci. Lorem ipsum dolor sit
-        amet, consectetur adipiscing elit. Mauris molestie congue augue, quis
-        feugiat risus. In hac habitasse platea dictumst. Vestibulum at lacinia
-        purus. Mauris sit amet posuere felis. Sed iaculis gravida turpis, ac
-        pharetra nisi. Sed porttitor sit amet nibh id ullamcorper. Pellentesque
-        eget fermentum justo, eget consequat nibh. Ut tempus ligula et urna
-        rutrum consectetur. Quisque risus nisl, consequat sed leo vel, commodo
-        bibendum libero. Duis lectus nulla, dapibus vel justo sed, hendrerit
-        accumsan tellus. Nunc faucibus laoreet velit eu pharetra. Sed neque
-        enim, blandit vel leo fermentum, porttitor ultrices nunc. Suspendisse
-        dapibus ultricies mauris, sed porta tortor. Vestibulum ante ipsum primis
-        in faucibus orci luctus et ultrices posuere cubilia curae; Aenean nec
-        nibh ac nunc accumsan dictum. Suspendisse potenti. Nullam vestibulum
-        euismod consequat. Mauris id magna sit amet dolor vulputate convallis.
-        Donec et efficitur orci. Aliquam porta at mi et lobortis. Proin eget
-        tristique dui. Quisque ultricies porta nisl, a mollis neque tempus id.
-        Curabitur dapibus a dui ut porta. Nulla dapibus porta molestie. Integer
-        et imperdiet metus, ut scelerisque neque. Etiam molestie odio quis
-        rutrum luctus. Suspendisse placerat ullamcorper orci, vel tincidunt nunc
-        maximus non. Mauris fringilla id diam condimentum congue. Fusce ut nibh
-        molestie, lobortis velit eget, volutpat mi. Morbi ultrices mauris sit
-        amet felis efficitur, quis tristique arcu sollicitudin. Vestibulum massa
-        enim, mollis eu nulla pellentesque, eleifend sollicitudin risus.
-        Suspendisse eget pulvinar tortor.
+    <div className="flex flex-1 flex-col p-10">
+      <h1 className=" py-6 text-4xl">About MAI</h1>
+      <p className="py-3 text-xl">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rhoncus
+        turpis sed lacus finibus, vitae vulputate lectus molestie. Ut eget neque
+        eget libero scelerisque pulvinar nec ac lorem. Integer in vestibulum
+        nisi. Donec et accumsan augue. Pellentesque finibus ante vestibulum,
+        vehicula purus ut, pellentesque mi. Nam id arcu lobortis, eleifend risus
+        at, aliquam sem. Nullam posuere ex massa, condimentum hendrerit tortor
+        iaculis quis. Donec interdum est non interdum mattis.
       </p>
-    </>
+      <p className="py-3 text-xl">
+        Duis auctor elit sit amet lobortis tristique. Aliquam id risus ut sem
+        pellentesque ullamcorper non quis erat. Ut sagittis hendrerit dui vel
+        pulvinar. Nulla sit amet finibus ligula. Sed aliquet mauris ante, quis
+        pharetra libero faucibus eu. Sed et ultrices massa. Suspendisse nibh
+        justo, rutrum vel leo sed, malesuada porta risus. Curabitur vestibulum
+        arcu nec metus tristique tristique. Nunc in risus ut nulla sagittis
+        fringilla. Nam at odio nec eros faucibus aliquet. Sed sit amet lacus
+        turpis. Nunc ultricies purus nunc, nec tincidunt lectus iaculis sed.
+      </p>
+      <p className="py-3 text-xl">
+        Morbi accumsan, risus at convallis dapibus, lacus odio ultrices mi, in
+        auctor lorem felis sit amet mauris. In molestie ullamcorper quam at
+        molestie. Mauris malesuada, enim quis congue congue, lacus urna pharetra
+        est, sed tincidunt lacus arcu nec arcu. Vivamus malesuada bibendum
+        augue, a mollis sem lobortis eu. Aliquam erat volutpat. Aliquam
+        tincidunt lorem non turpis ultrices eleifend. Ut at est in nisi tempus
+        cursus. Nullam iaculis massa a maximus porta.
+      </p>
+    </div>
   );
 }

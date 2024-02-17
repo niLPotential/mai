65847f411c25a210d67dfb7c861aa27381c2beab
niLPotential <niLPotential@proton.me>
2024-02-17 13:24:49 +0900

    Add cards example for subscribe page

--- app/shop/subscribe/page.tsx
+++ app/shop/subscribe/page.tsx
@@ -1,3 +1,27 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Page() {
-  return <div>Subscribe page</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Free Tier</CardTitle>
+        <CardDescription>Allows for 10 requests per week</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content 1</p>
+        <p>Card Content 2</p>
+        <p>Card Content 3</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
 }

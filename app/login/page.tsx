1cdf3e180354d4432de219d5e32e0e4ee97ad3c3
niLPotential <niLPotential@proton.me>
2024-02-17 14:03:19 +0900

    Add demo warning to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,36 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 export default function Page() {
   return (
     <form>
       <input type="email" name="email" placeholder="Email" required />
       <input type="password" name="password" placeholder="Password" required />
-      <button type="submit">Login</button>
+      <AlertDialog>
+        <AlertDialogTrigger type="submit">Sign In</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>This is currently in demo</AlertDialogTitle>
+            <AlertDialogDescription>
+              This site is a demo project. Signing in is currently not
+              supported.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </form>
   );
 }

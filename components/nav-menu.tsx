3495985ab30a3b10904165883181b305c072b994
niLPotential <niLPotential@proton.me>
2024-02-17 18:34:39 +0900

    Flex nav menu

--- components/nav-menu.tsx
+++ components/nav-menu.tsx
@@ -9,53 +9,59 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 import { ModeToggle } from "@/components/mode-toggle";
 
 export function NavMenu() {
   return (
     <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink href="/">Home</NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink href="/compose">Compose</NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Shop</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul>
-              <li>
-                <NavigationMenuLink href="/shop/tokens">
-                  Tokens
-                </NavigationMenuLink>
-              </li>
-              <li>
-                <NavigationMenuLink href="/shop/subscribe">
-                  Suscription
-                </NavigationMenuLink>
-              </li>
-              <li>
-                <NavigationMenuLink href="/shop/promotion">
-                  Promotion
-                </NavigationMenuLink>
-              </li>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink href="/about">About</NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink href="/account">Account</NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink href="/login">Sign In</NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <ModeToggle />
-        </NavigationMenuItem>
+      <NavigationMenuList className=" w-screen p-5">
+        <div className="flex flex-1 items-center gap-5">
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/">Home</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/compose">Compose</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="relative">
+            <NavigationMenuTrigger>Shop</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="flex flex-col gap-3 p-3">
+                <li>
+                  <NavigationMenuLink href="/shop/tokens">
+                    Tokens
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink href="/shop/subscribe">
+                    Suscription
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink href="/shop/promotion">
+                    Promotion
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+            <NavigationMenuViewport />
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/about">About</NavigationMenuLink>
+          </NavigationMenuItem>
+        </div>
+        <div className="flex items-center gap-5">
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/account">Account</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/login">Sign In</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <ModeToggle />
+          </NavigationMenuItem>
+        </div>
       </NavigationMenuList>
     </NavigationMenu>
   );

4b361e44e971f0d7083fe61c682ec1255240bb6f
niLPotential <niLPotential@proton.me>
2024-02-18 20:00:25 +0900

    Add card to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col p-5 md:px-72">
-      <h1 className="py-10 text-center text-6xl">Welcome to MAI</h1>
-      <h2 className="py-5 text-center text-4xl">
-        Using AI generated music for all
-      </h2>
-      <p className="py-2 text-2xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac aliquet
-        elit, nec malesuada ex. Suspendisse rhoncus mi massa, vel mollis nisl
-        ornare in. Phasellus lobortis arcu dui, vel consequat odio vulputate
-        laoreet. Etiam eu commodo leo. Nulla in metus vitae sapien posuere
-        ornare vel vel mauris. Sed sit amet volutpat enim, sit amet fermentum
-        justo. In aliquet dolor lorem, sit amet consequat ligula imperdiet non.
-      </p>
+    <div className="flex flex-col items-center justify-center p-5 md:w-1/2">
+      <Card className="flex flex-col justify-center text-center">
+        <CardHeader>
+          <CardDescription>BACK GROUND</CardDescription>
+          <CardTitle>MAI MUSIC</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>
+            An AI compositional feature that allows for quick, easy, accurate
+            creation of music, using a variety of specific criteria. Create tons
+            of songs in under a minute with a more intuitive and simple way to
+            compose.
+          </p>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button>Start Now</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }

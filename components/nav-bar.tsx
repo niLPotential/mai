4bbe6db6186666ea19511a0574b5f719da4ee3a6
niLPotential <niLPotential@proton.me>
2024-02-18 19:59:55 +0900

    Add nav bar components directly

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -5,43 +5,22 @@ import React from "react";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
 
-const barContent: { href: string; text: string }[] = [
-  {
-    href: "/",
-    text: "Home",
-  },
-  {
-    href: "/compose",
-    text: "Make Music",
-  },
-  {
-    href: "/about",
-    text: "About",
-  },
-  {
-    href: "/shop",
-    text: "Market",
-  },
-  {
-    href: "/account",
-    text: "My Page",
-  },
-  {
-    href: "/contact",
-    text: "Contact",
-  },
-];
-
 export function NavBar() {
   return (
     <div className="my-5 flex h-5 justify-center gap-5">
       <Separator orientation="vertical" />
-      {barContent.map(({ href, text }) => (
-        <>
-          <Link href={href}>{text}</Link>
-          <Separator orientation="vertical" />
-        </>
-      ))}
+      <Link href="/">Home</Link>
+      <Separator orientation="vertical" />
+      <Link href="/compose">Make Music</Link>
+      <Separator orientation="vertical" />
+      <Link href="/about">About</Link>
+      <Separator orientation="vertical" />
+      <Link href="/shop">Market</Link>
+      <Separator orientation="vertical" />
+      <Link href="/account">My Page</Link>
+      <Separator orientation="vertical" />
+      <Link href="/contact">Contact</Link>
+      <Separator orientation="vertical" />
     </div>
   );
 }
